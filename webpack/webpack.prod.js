278599a0c0a92c4f356031dcfda07451cc4c7e97
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-10 15:11:33 +0300

    fix: add public path for deploy

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mode: 'production',
-  devtool: false,
-}
+	mode: 'production',
+	devtool: false,
+	output: {
+		publicPath: '/share-location/', // публичный путь для GitHub Pages
+	},
+};
