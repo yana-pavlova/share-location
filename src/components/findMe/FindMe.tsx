7afc9961ddb1e86f9250ba6a4807397a71288ea9
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-22 22:30:26 +0300

    feat: always fetch a new location when clicking on button

--- src/components/findMe/FindMe.tsx
+++ src/components/findMe/FindMe.tsx
@@ -3,34 +3,27 @@ import styles from './findMe.module.scss';
 import { fetchLocation } from '../../utils/api';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import {
-	addCurrentLocation,
-	selectCurrentLocation,
-} from '../../state/markersSlice';
-import { useSelector, useDispatch } from 'react-redux';
+import { addCurrentLocation } from '../../state/markersSlice';
+import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
 const FindMe = () => {
 	const dispatch = useDispatch();
 	const map = useMap();
-	const curLoc = useSelector(selectCurrentLocation);
 
 	const getLocation = async () => {
-		if (!curLoc) {
-			const coords = await fetchLocation();
-			if (coords) {
-				dispatch(
-					addCurrentLocation({
-						position: [coords.latitude, coords.longitude],
-						text: "You're here",
-						id: uuidv4(),
-					})
-				);
+		const coords = await fetchLocation();
+		if (coords) {
+			dispatch(
+				addCurrentLocation({
+					position: [coords.latitude, coords.longitude],
+					text: "You're here",
+					id: uuidv4(),
+				})
+			);
 
-				map.setView(L.latLng(coords.latitude, coords.longitude), map.getZoom());
-			}
+			map.setView(L.latLng(coords.latitude, coords.longitude), map.getZoom());
 		}
-		if (curLoc) map.setView(L.latLng(curLoc[0], curLoc[1]), map.getZoom());
 	};
 
 	return (
