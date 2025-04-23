992815ff6b0b0c9029c60716fa44f4d9bf91e2e1
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-23 17:13:48 +0300

    fix: use i18n in api errors & fix a type in api function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,11 @@
 import { toast } from 'react-toastify';
 import { TAddress } from '../types';
 import geoTimeOut from './geoTimeOut';
+import { TFunction } from 'i18next';
 
-export const fetchLocation = async (): Promise<
-	{ latitude: number; longitude: number } | undefined
-> => {
+export const fetchLocation = async (
+	t: TFunction<'translation', undefined>
+): Promise<{ latitude: number; longitude: number } | undefined> => {
 	let loadingToastId;
 
 	try {
@@ -14,17 +15,13 @@ export const fetchLocation = async (): Promise<
 		return { latitude, longitude };
 	} catch (err: any) {
 		if (err.code === 1) {
-			toast.error(
-				'Please, enable geolocation on your device or browser. Then try again'
-			);
+			toast.error(t('forbidfLocationErrorText'));
 		} else if (err.code === 2) {
-			toast.error(
-				'Unfortunately, we could not get your location. Please, try again'
-			);
+			toast.error(t('locationGeneralErrorText'));
 		} else if (err.code === 3) {
-			toast.error('Location request timed out. Please, try again');
+			toast.error(t('locationRequestTimeoutErrorText'));
 		} else {
-			toast.error(`Something went wrong. Please, try again`);
+			toast.error(t('locationUndefinedErrorText'));
 		}
 		return undefined;
 	} finally {
@@ -45,7 +42,7 @@ export const fetchAddress = async (
 	return data.address;
 };
 
-export const searchAdress = async (query: string): Promise<any> => {
+export const searchAddress = async (query: string): Promise<any> => {
 	const res = await fetch(
 		'https://nominatim.openstreetmap.org/search?format=json&q=' + query
 	);
