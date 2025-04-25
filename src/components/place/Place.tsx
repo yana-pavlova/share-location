d523609ad31ece8938046b01935ff0e53a2f5f80
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-25 20:00:17 +0300

    refactor: decompose places

---
+++ src/components/place/Place.tsx
@@ -0,0 +1,77 @@
+import clsx from 'clsx';
+import { Copy, Trash2 } from 'lucide-react';
+import styles from './Place.module.scss';
+import { TMarker } from '../../types';
+import { useCopyLink } from '../../hooks/useCopyLink';
+
+interface PlaceProps {
+	marker: TMarker;
+	onClick: (e: React.MouseEvent<HTMLLIElement>) => void;
+	onRemove: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const Place = ({ marker, onClick, onRemove }: PlaceProps) => {
+	const copyLink = useCopyLink();
+
+	const onCopy = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		let url = window.location.origin;
+		if (url.includes('github')) url += '/share-location';
+		const target = e.target as HTMLElement;
+		const coords = target.closest('li')?.dataset.coords;
+
+		const lat = coords?.split(',')[0];
+		const lng = coords?.split(',')[1];
+		const textToCopy = `${url}?lat=${lat}&lng=${lng}`;
+
+		copyLink(textToCopy);
+	};
+
+	const onMouseEnter = (e: React.MouseEvent<HTMLLIElement>) => {
+		const target = e.target as HTMLElement;
+		const liElement = target.closest('li');
+		liElement?.classList.add('li-highlighted');
+		const marker = document.querySelector(
+			`.img-${liElement?.id}`
+		) as HTMLElement;
+		if (marker) marker.style.opacity = '1';
+	};
+
+	const onMouseLeave = (e: React.MouseEvent<HTMLLIElement>) => {
+		const target = e.target as HTMLElement;
+		const liElement = target.closest('li');
+		liElement?.classList.remove('li-highlighted');
+		const marker = document.querySelector(
+			`.img-${liElement?.id}`
+		) as HTMLElement;
+		if (marker) marker.style.opacity = '0.5';
+	};
+
+	return (
+		<li
+			id={marker.id}
+			data-coords={marker.position.toString()}
+			className={clsx(styles.marker, 'li-normal')}
+			key={marker.id}
+			onClick={onClick}
+		>
+			<span onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+				{marker.text}
+			</span>
+			<button
+				className={`${styles.copyLinkButton} ${styles.button}`}
+				onClick={onCopy}
+			>
+				<Copy size={20} color="#000" />
+				Поделиться
+			</button>
+			<button
+				className={`${styles.removeMarkerButton} ${styles.button}`}
+				onClick={onRemove}
+			>
+				<Trash2 size={20} color="#000" />
+				Удалить
+			</button>
+		</li>
+	);
+};
--- src/components/places/Places.tsx
+++ src/components/places/Places.tsx
@@ -1,5 +1,4 @@
 import styles from './places.module.scss';
-import clsx from 'clsx';
 import {
 	selectMarkers,
 	removeMarker,
@@ -7,9 +6,8 @@ import {
 } from '../../state/markersSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLayoutEffect, useRef, useState } from 'react';
-import { useCopyLink } from '../../hooks/useCopyLink';
-import { Trash2, Copy } from 'lucide-react';
 import { ConfirmationModal } from '../confirmationModal/confirmationModal';
+import { Place } from '../place/Place';
 
 type PlacesProps = {
 	mapRef: React.MutableRefObject<L.Map | null>;
@@ -20,7 +18,6 @@ const Places = ({ mapRef }: PlacesProps) => {
 	const confirmActionRef = useRef<() => void>(() => {});
 	const dispatch = useDispatch();
 	const markers = useSelector(selectMarkers);
-	const copyLink = useCopyLink();
 	const listRef = useRef<HTMLUListElement>(null);
 
 	useLayoutEffect(() => {
@@ -57,20 +54,6 @@ const Places = ({ mapRef }: PlacesProps) => {
 		}
 	};
 
-	const handleCopyLinkClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.stopPropagation();
-		let url = window.location.origin;
-		if (url.includes('github')) url += '/share-location';
-		const target = e.target as HTMLElement;
-		const coords = target.closest('li')?.dataset.coords;
-
-		const lat = coords?.split(',')[0];
-		const lng = coords?.split(',')[1];
-		const textToCopy = `${url}?lat=${lat}&lng=${lng}`;
-
-		copyLink(textToCopy);
-	};
-
 	const handleLiCLick = (e: React.MouseEvent<HTMLLIElement>) => {
 		const target = e.target as HTMLElement;
 		const liElement = target.closest('li');
@@ -84,60 +67,18 @@ const Places = ({ mapRef }: PlacesProps) => {
 		}
 	};
 
-	const handleMouseLeave = (e: React.MouseEvent<HTMLLIElement>) => {
-		const target = e.target as HTMLElement;
-		const liElement = target.closest('li');
-		liElement?.classList.remove('li-highlighted');
-		const marker = document.querySelector(
-			`.img-${liElement?.id}`
-		) as HTMLElement;
-		if (marker) marker.style.opacity = '0.5';
-	};
-
-	const handleMouseEnter = (e: React.MouseEvent<HTMLLIElement>) => {
-		const target = e.target as HTMLElement;
-		const liElement = target.closest('li');
-		liElement?.classList.add('li-highlighted');
-		const marker = document.querySelector(
-			`.img-${liElement?.id}`
-		) as HTMLElement;
-		if (marker) marker.style.opacity = '1';
-	};
-
 	return (
 		<>
 			{markers.length > 1 && (
 				<ul ref={listRef} className={styles.list}>
 					{markers.map((marker) => {
 						return (
-							!marker.currentLocation && (
-								<li
-									id={marker.id}
-									data-coords={marker.position.toString()}
-									className={clsx(styles.marker, 'li-normal')}
-									key={marker.id}
-									onClick={handleLiCLick}
-								>
-									<span
-										onMouseEnter={handleMouseEnter}
-										onMouseLeave={handleMouseLeave}
-									>
-										{marker.text}
-									</span>
-									<button
-										className={`${styles.copyLinkButton} ${styles.button}`}
-										onClick={handleCopyLinkClick}
-									>
-										<Copy size={20} color="#000" />
-									</button>
-									<button
-										className={`${styles.removeMarkerButton} ${styles.button}`}
-										onClick={handleRemoveMarkerClick}
-									>
-										<Trash2 size={20} color="#000" />
-									</button>
-								</li>
-							)
+							<Place
+								key={marker.id}
+								marker={marker}
+								onClick={handleLiCLick}
+								onRemove={handleRemoveMarkerClick}
+							/>
 						);
 					})}
 				</ul>
