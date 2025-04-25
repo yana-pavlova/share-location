6c5883f97c1f8d74495e90ea928685c837625980
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-25 12:48:07 +0300

    chore: remove chain operator (no need anymore)

--- src/utils/concatenateAddress.ts
+++ src/utils/concatenateAddress.ts
@@ -5,13 +5,13 @@ export const concatenateAddress = (address: TAddress | null) => {
 	if (address === null) return t('unknownAddress');
 
 	const addressText = [
-		address?.city ||
-			address?.town ||
-			address?.village ||
-			address?.hamlet ||
-			address?.municipality,
-		address?.road,
-		address?.house_number,
+		address.city ||
+			address.town ||
+			address.village ||
+			address.hamlet ||
+			address.municipality,
+		address.road,
+		address.house_number,
 	]
 		.filter(Boolean)
 		.join(', ');
